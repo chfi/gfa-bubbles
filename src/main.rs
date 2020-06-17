f6abcafd3abd281234c30c5d709b604903749b2e
Christian <christian@chfi.se>
2020-06-17 15:25:22 -0500

    Correctly detect the starts of bubbles in the sample path

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use gfa::parser::parse_gfa;
 use std::env;
 use std::path::PathBuf;
 
+use std::collections::BTreeSet;
+use std::collections::VecDeque;
+
 use std::convert::TryFrom;
 use std::process::exit;
 
@@ -28,9 +31,31 @@ fn handlegraph_to_pet<T: HandleGraph>(graph: &T) -> DiGraph<u32, ()> {
     }))
 }
 
-fn usage(name: &str) {
-    println!("Usage: {} <path-to-gfa>", name);
-    exit(1);
+fn bubble_starts<T: HandleGraph>(graph: &T) -> Vec<NodeId> {
+    let mut starts = Vec::new();
+
+    let mut visited: BTreeSet<NodeId> = BTreeSet::new();
+
+    let mut deque: VecDeque<NodeId> = VecDeque::new();
+
+    deque.push_back(NodeId::from(1));
+
+    while let Some(nid) = deque.pop_front() {
+        let h = Handle::pack(nid, false);
+        if !visited.contains(&nid) {
+            if graph.get_degree(&h, Direction::Right) > 1 {
+                println!("start at {:?}", nid);
+                starts.push(nid)
+            }
+            handle_edges_iter(graph, h, Direction::Right).for_each(|h| {
+                deque.push_back(h.id());
+                visited.insert(nid);
+            });
+        }
+        visited.insert(nid);
+    }
+
+    starts
 }
 
 fn main() {
@@ -42,13 +67,40 @@ fn main() {
     let path = PathBuf::from(args[1].clone());
     if let Some(gfa) = parse_gfa(&path) {
         let graph = HashGraph::from_gfa(&gfa);
-        let pet = handlegraph_to_pet(&graph);
 
-        let mut dfs = Dfs::new(&pet, NodeIndex::new(1));
-        while let Some(nx) = dfs.next(&pet) {
-            println!("{:?}", nx);
-        }
+        let starts = bubble_starts(&graph);
+
+        println!("Found {} bubble starts", starts.len());
+
+    // let pet = handlegraph_to_pet(&graph);
+
+    // let mut dfs = Dfs::new(&pet, NodeIndex::new(1));
+    // while let Some(nx) = dfs.next(&pet) {
+    //     println!("{:?}", nx);
+    // }
+
+    // let mst = min_spanning_tree(&pet);
+
+    // for n in &mst {
+    //     println!("{:?}", n);
+    // }
+    // let mst_graph: DiGraph<u32, ()> = Graph::from_elements(mst);
+
+    // let mut dfs = Dfs::new(&mst_graph, NodeIndex::new(1));
+    // while let Some(nx) = dfs.next(&mst_graph) {
+    //     println!("{:?}", nx);
+    // }
+
+    // let mut bfs = Bfs::new(&mst_graph, NodeIndex::new(1));
+    // while let Some(nx) = bfs.next(&mst_graph) {
+    //     println!("{:?}", nx);
+    // }
     } else {
         usage(&args[0]);
     }
+}
+
+fn usage(name: &str) {
+    println!("Usage: {} <path-to-gfa>", name);
+    exit(1);
 }
