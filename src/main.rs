1c877c0557ffb005aa25c68e843eb6f16ec4fcc4
Christian <christian@chfi.se>
2020-06-23 18:28:04 -0500

    Almost working! Just need to fine-tune it a bit

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ impl BubbleState {
         let mut branch_ends = BTreeMap::new();
         for n in nodes {
             branch_visits.insert(*n, BTreeSet::new());
-            branch_deque.insert(*n, VecDeque::new());
+            branch_deque.insert(*n, VecDeque::from(vec![*n]));
             branch_ends.insert(*n, BTreeSet::new());
         }
         BubbleState {
@@ -61,77 +61,60 @@ impl BubbleState {
         }
     }
 
-    // Update the visited set and queue for the branch that started at
-    // the node `from`, with the visited node `visited`, which has the
-    // right-hand neighbors `neighbors`
-
-    // TODO this should take the LHS-degree of the visited node and update the branch_ends accordingly, if it matches the bubble degree
-    #[allow(dead_code)]
-    fn visit_from_branch(
-        &mut self,
-        from: NodeId,
-        visited: NodeId,
-        neighbors: &Vec<NodeId>,
-    ) {
-        self.branch_visits.entry(from).and_modify(|set| {
-            set.insert(visited);
-        });
-
-        let visits = self.branch_visits.get(&from).unwrap();
-        let to_visit = neighbors.into_iter().filter(|n| !visits.contains(&n));
-
-        self.branch_deque.entry(from).and_modify(|deq| {
-            for n in to_visit {
-                deq.push_back(*n);
-            }
-        });
-    }
-
     fn can_continue(&self) -> bool {
-        self.branch_deque.values().all(|dq| dq.is_empty())
+        !self.branch_deque.values().all(|dq| dq.is_empty())
     }
 
-    // Returns the possibly detected ends
+    // Iterate each of the search branches one step, returning the set of
+    // possible end nodes, if any were found
     fn propagate<T: HandleGraph>(
         &mut self,
         graph: &T,
     ) -> Option<BTreeSet<NodeId>> {
         // take one step on each of the branches
 
-        // println!("propagating");
         let branches: Vec<_> = self.branch_deque.keys().cloned().collect();
 
         let mut possible_ends: BTreeSet<NodeId> = BTreeSet::new();
 
+        // println!("propagating");
+
         for b_id in branches.into_iter() {
             let b_deq: &mut VecDeque<NodeId> =
                 self.branch_deque.get_mut(&b_id).unwrap();
-            println!("deq len: {}", b_deq.len());
 
             if let Some(next_id) = b_deq.pop_front() {
-                println!("branch {:?}\t visiting {:?}", b_id, next_id);
                 let b_visits: &mut BTreeSet<NodeId> =
                     self.branch_visits.get_mut(&b_id).unwrap();
 
                 if !b_visits.contains(&next_id) {
                     let handle = Handle::pack(next_id, false);
 
-                    if graph.get_degree(&handle, Direction::Left) > 1 {
-                        println!("possible end");
-                        possible_ends.insert(next_id);
+                    let out_degree =
+                        graph.get_degree(&handle, Direction::Right);
+
+                    if out_degree == 1 {
+                        let possible_end =
+                            handle_edges_iter(graph, handle, Direction::Right)
+                                .next()
+                                .unwrap();
+                        possible_ends.insert(possible_end.id());
                         self.branch_ends
                             .get_mut(&b_id)
                             .unwrap()
-                            .insert(next_id);
+                            .insert(possible_end.id());
                     }
 
-                    let neighbors =
+                    let neighbors: Vec<_> =
                         handle_edges_iter(graph, handle, Direction::Right)
-                            .filter(|h| !b_visits.contains(&h.id()));
+                            .collect();
+
+                    // println!("neighbors: {}", neighbors.len());
 
                     for h in neighbors {
                         b_deq.push_back(h.id());
                     }
+                    // println!("deque next: {}", b_deq.len());
 
                     b_visits.insert(next_id);
                 }
@@ -141,14 +124,14 @@ impl BubbleState {
         if possible_ends.is_empty() {
             None
         } else {
-            println!("possible ends: {}", possible_ends.len());
             Some(possible_ends)
         }
     }
 
-    // if the given node exists in all branch_ends, return the corresponding bubble
+    // Check if the given node exists in all branch_ends, return the
+    // corresponding bubble
     fn check_finished(&self, node: &NodeId) -> Option<Bubble> {
-        let count = self.branch_ends.iter().fold(0, |a, (branch, ends)| {
+        let count = self.branch_ends.iter().fold(0, |a, (_branch, ends)| {
             let ends: &BTreeSet<NodeId> = ends;
             if ends.contains(node) {
                 a + 1
@@ -173,14 +156,11 @@ struct Bubble {
     end: NodeId,
 }
 
-fn bubble_starts<T: HandleGraph>(graph: &T) -> Vec<Bubble> {
-    let mut starts = Vec::new();
-
+fn find_bubbles<T: HandleGraph>(graph: &T) -> Vec<Bubble> {
     let mut visited: BTreeSet<NodeId> = BTreeSet::new();
 
     let mut deque: VecDeque<NodeId> = VecDeque::new();
 
-    let mut current_bubble: Option<BubbleState> = None;
     let mut bubbles: Vec<Bubble> = Vec::new();
 
     deque.push_back(NodeId::from(1));
@@ -189,7 +169,7 @@ fn bubble_starts<T: HandleGraph>(graph: &T) -> Vec<Bubble> {
         let h = Handle::pack(nid, false);
         if !visited.contains(&nid) {
             let rhs_degree = graph.get_degree(&h, Direction::Right);
-            if current_bubble.is_none() && rhs_degree > 1 {
+            if rhs_degree > 1 {
                 println!("start at {:?}", nid);
                 // start a new bubble
                 let neighbors = handle_edges_iter(graph, h, Direction::Right)
@@ -206,24 +186,17 @@ fn bubble_starts<T: HandleGraph>(graph: &T) -> Vec<Bubble> {
                         // bubble, we're done
                         for end in possible_ends {
                             if let Some(b) = bubble.check_finished(&end) {
+                                println!("end at {:?}", b.end);
                                 running = false;
                                 bubbles.push(b);
                             }
                         }
                     }
+                    if !bubble.can_continue() {
+                        println!("aborting");
+                        running = false;
+                    }
                 }
-
-                // TODO loop through all the queues in the
-                // bubblestate, traversing the graph and updating the
-                // visited sets of each branch
-
-                // whenever a node is found with the same incoming degree as rhs_degree, add it to the branch_ends
-
-                // then check if that node exists across all branch_ends; if it does, the bubble is done
-
-                // current_bubble = Some(BubbleState::new(&neighbors));
-
-                starts.push(nid)
             }
             handle_edges_iter(graph, h, Direction::Right).for_each(|h| {
                 deque.push_back(h.id());
@@ -231,9 +204,7 @@ fn bubble_starts<T: HandleGraph>(graph: &T) -> Vec<Bubble> {
         }
         visited.insert(nid);
     }
-
     bubbles
-    // starts
 }
 
 fn main() {
@@ -246,33 +217,13 @@ fn main() {
     if let Some(gfa) = parse_gfa(&path) {
         let graph = HashGraph::from_gfa(&gfa);
 
-        let starts = bubble_starts(&graph);
+        let bubbles = find_bubbles(&graph);
 
-        println!("Found {} bubble starts", starts.len());
+        println!("Found {} bubbles", bubbles.len());
 
-    // let pet = handlegraph_to_pet(&graph);
-
-    // let mut dfs = Dfs::new(&pet, NodeIndex::new(1));
-    // while let Some(nx) = dfs.next(&pet) {
-    //     println!("{:?}", nx);
-    // }
-
-    // let mst = min_spanning_tree(&pet);
-
-    // for n in &mst {
-    //     println!("{:?}", n);
-    // }
-    // let mst_graph: DiGraph<u32, ()> = Graph::from_elements(mst);
-
-    // let mut dfs = Dfs::new(&mst_graph, NodeIndex::new(1));
-    // while let Some(nx) = dfs.next(&mst_graph) {
-    //     println!("{:?}", nx);
-    // }
-
-    // let mut bfs = Bfs::new(&mst_graph, NodeIndex::new(1));
-    // while let Some(nx) = bfs.next(&mst_graph) {
-    //     println!("{:?}", nx);
-    // }
+        for b in bubbles {
+            println!("{:?} -> {:?}", b.start, b.end);
+        }
     } else {
         usage(&args[0]);
     }
